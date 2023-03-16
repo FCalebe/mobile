8794c8f791cf00727adb1ea508487f5bb5c59ea2
fernandocalebe <fernandocalebe14@gmail.com>
2023-03-16 20:56:50 -0300

    aula2

---
+++ aula1.js
@@ -0,0 +1,32 @@
+var nome1 = 'Joao'
+nome1 = 'Mateus'
+let nome2 = 'Jose'
+nome2 = 'Felipe'
+
+const salario =1300;
+
+
+var nome = 'Calebe';
+let idade = '22';
+const sexo = 'masculino';
+
+const usuario = ['Calebe', '22', true];
+
+console.log(usuario);
+VM522:1 (3) ['Calebe', '22', true]
+
+const pincel= {
+    cor : 'azul',
+    fabricante : 'Pilot',
+    tipo : '1',
+    espessuraDaPonta : '1.45'
+}
+
+pincel
+{cor : 'azul', fabricante = 'Pilot', tipo = '1', espessuraDaPonta = '1.45'}
+
+
+caixa ={
+    formato : 'retangular',
+    itens : 'pincel'
+}
